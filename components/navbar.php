9576a73d2e335586b2a3198a868ae0c1fa378468
Feny <ehab1nmy@gmail.com>
2025-01-24 19:24:50 +0300

    Implement user session handling in navbar and update index structure

--- components/navbar.php
+++ components/navbar.php
@@ -1,17 +1,37 @@
+<?php
+  session_start();
+  // Example user data
+  $_SESSION['user'] = [
+    'username' => 'John Doe',
+    'role' => 'Admin'
+  ];
+  $loggedIn = isset($_SESSION['user']);
+?>
+
 <head>
   <link rel="stylesheet" href="../styles/navbar.css">
 </head>
 <header>
-  <div class="user">
-    <img src="../images/purple2.png" alt="user image" />
-    <div class="info">
-      <h2 class="username">اسم المستخدم</h2>
-      <p class="role">معلم / طالب</p>
+  <?php if ($loggedIn) { ?>
+    <div class="user">
+      <img src="../images/purple2.png" alt="user image" />
+      <div class="info">
+        <h2 class="username"><?php echo $_SESSION['user']['username']; ?></h2>
+        <p class="role"><?php echo $_SESSION['user']['role']; ?></p>
+      </div>
     </div>
-  </div>
-  <nav>
-    <a href="#">الفصول</a>
-    <a href="#">المجموعات</a>
-    <a href="#">الإعدادات</a>
-  </nav>
+    <nav>
+      <a href="#">الفصول</a>
+      <a href="#">المجموعات</a>
+      <a href="#">الإعدادات</a>
+    </nav>
+  <?php } else { ?>
+    <nav>
+      <a href="#">الرئيسية</a>
+      <a href="#">الفصول</a>
+      <a href="#">المجموعات</a>
+      <a href="#">التسجيل</a>
+      <a href="#">الدخول</a>
+    </nav>
+  <?php } ?>
 </header>
