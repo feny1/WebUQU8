f29e235948dc895d246251444a8ed24256ecc330
zyadfallatahUQU <zyadfallatah@gmail.com>
2025-01-24 18:40:54 +0300

    seperate navbar into isolated component

--- pages/viewClasses.php
+++ pages/viewClasses.php
@@ -9,20 +9,7 @@
 </head>
 
 <body>
-  <header>
-    <div class="user">
-      <img src="../images/purple2.png" alt="user image" />
-      <div class="info">
-        <h2 class="username">اسم المستخدم</h2>
-        <p class="role">معلم / طالب</p>
-      </div>
-    </div>
-    <nav>
-      <a href="#">الفصول</a>
-      <a href="#">المجموعات</a>
-      <a href="#">الإعدادات</a>
-    </nav>
-  </header>
+  <?php include "../components/navbar.php" ?>
 
   <h1 class="heading">الفصول الدراسية</h1>
 
