bda72181baf756e689a335340efc8634d6fa010a
zyadfallatahUQU <zyadfallatah@gmail.com>
2025-01-24 17:19:49 +0300

    fixed the behaveiour of group evaluation

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const grading = document.querySelector("#grading");
 
-let lastValidGrading = null;
+let lastValidGrading = 0;
 
 grading.addEventListener("input", (e) => {
+  if (lastValidGrading === 0) {
+    e.currentTarget.value = e.data;
+  }
+
+  if (e.inputType === "deleteContentBackward" && e.currentTarget.value === "") {
+    e.currentTarget.value = 0;
+    lastValidGrading = 0;
+    return;
+  }
+
   if (e.currentTarget.value === "") {
     e.currentTarget.value = lastValidGrading ?? null;
     return;
@@ -12,9 +22,9 @@ grading.addEventListener("input", (e) => {
     e.currentTarget.valueAsNumber < 1 ||
     e.currentTarget.valueAsNumber > 100
   ) {
-    e.currentTarget.value = lastValidGrading ?? null;
+    e.currentTarget.value = Number.parseInt(lastValidGrading) ?? null;
     return;
   }
 
-  if (e.currentTarget) lastValidGrading = e.currentTarget.value;
+  lastValidGrading = e.currentTarget.value;
 });
